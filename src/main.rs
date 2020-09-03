5fdd3725a1e90b2396fe3a99d4b22b83590cbf66
Alex Sun <alexsun82@icloud.com>
2020-09-03 11:51:21 +0200

    Breaking changes :)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use regex::Regex;
 use std::env;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::str;
-// use std::thread;
 
-#[derive(Debug)]
 struct Connection {
     server: String,
     port: u16,
@@ -25,7 +22,7 @@ impl Connection {
 
     fn parse_channel(channel: String) -> String {
         match channel {
-            c if c.starts_with("#") => c,
+            c if c.starts_with('#') => c,
             c => format!("#{}", c),
         }
     }
@@ -35,70 +32,17 @@ impl Connection {
     }
 }
 
-enum MsgCmd {
-    PRIVMSG,
-    MOTD,
-    PING,
-    Other,
-}
-
 struct Message {
-    cmd: MsgCmd,
-    source: String,
-    target: String,
+    header: String,
     content: String,
 }
 
 impl Message {
-    pub fn from_buffer(buf: &[u8; 512]) -> Option<Self> {
+    pub fn from_buffer(buf: &[u8; 512]) -> Self {
         let as_utf8 = str::from_utf8(buf).expect("Invalid Message");
-        let mut split_iter = as_utf8.split_whitespace();
-
-        let source = match split_iter.next() {
-            Some(src) => src.split("!").collect::<Vec<_>>()[0].to_owned(),
-            None => return None,
-        };
-
-        let message = match split_iter.next() {
-            Some("PING") => Message {
-                cmd: MsgCmd::PING,
-                source,
-                target: match split_iter.next() {
-                    Some(tg) => tg.to_owned(),
-                    None => String::new(),
-                },
-                content: split_iter.collect::<Vec<&str>>().join(" "),
-            },
-            Some("MOTD") => Message {
-                cmd: MsgCmd::MOTD,
-                source,
-                target: match split_iter.next() {
-                    Some(tg) => tg.to_owned(),
-                    None => String::new(),
-                },
-                content: split_iter.collect::<Vec<&str>>().join(" "),
-            },
-            Some("PRIVMSG") => Message {
-                cmd: MsgCmd::PRIVMSG,
-                source,
-                target: match split_iter.next() {
-                    Some(tg) => tg.to_owned(),
-                    None => String::new(),
-                },
-                content: split_iter.collect::<Vec<&str>>().join(" "),
-            },
-            Some(_) => Message {
-                cmd: MsgCmd::Other,
-                source,
-                target: match split_iter.next() {
-                    Some(tg) => tg.to_owned(),
-                    None => String::new(),
-                },
-                content: split_iter.collect::<Vec<&str>>().join(" "),
-            },
-            None => return None,
-        };
-        Some(message)
+        let split = as_utf8.split(':').collect::<Vec<_>>();
+        let (header, content) = (split[1].to_owned(), split[2].to_owned());
+        Message { header, content }
     }
 }
 
@@ -108,8 +52,8 @@ fn format_cmd(cmd: &str, msg: &str) -> Box<[u8]> {
 }
 
 fn send_auth(conn: &Connection, stream: &mut TcpStream) -> std::io::Result<()> {
-    stream.write(&*format_cmd("NICK", &conn.username))?;
-    stream.write(&*format_cmd(
+    stream.write_all(&*format_cmd("NICK", &conn.username))?;
+    stream.write_all(&*format_cmd(
         "USER",
         &format!("{0} * * :{0}", &conn.username),
     ))?;
@@ -130,37 +74,43 @@ fn main() -> std::io::Result<()> {
         println!("Connected to {}", &conn.server);
         let mut buf;
 
-        // Welcome loop
         loop {
             buf = [0; 512];
             stream.read(&mut buf)?;
-            // let message = str::from_utf8(&buf).expect("Invalid message");
-            if let Some(message) = Message::from_buffer(&buf) {
-                match message.cmd {
-                    MsgCmd::PING => {
-                        println!("Recevied a PING");
-                        stream.write(&*format_cmd("PONG", &message.content))?;
-                        println!("Sent a PONG");
-                    }
-                    MsgCmd::MOTD => {
-                        println!("MOTD - {}", &message.content);
-                    }
-                    MsgCmd::PRIVMSG => {
-                        println!("{} -> {}", &message.source, &message.content);
-                    }
-                    MsgCmd::Other => {
-                        if message.content.contains("No Ident response") {
-                            send_auth(&conn, &mut stream)?;
-                        } else if message.content.contains("376") {
-                            stream.write(&*format_cmd("JOIN", &conn.channel))?;
-                        }
-                    }
+            let message = Message::from_buffer(&buf);
+
+            println!("{}", &message.content);
+
+            match message {
+                Message { header, content } if header.contains("PING") => {
+                    println!("Recevied a PING");
+                    stream.write_all(&*format_cmd("PONG", &content))?;
+                    println!("Sent a PONG");
                 }
+                Message { header, content } if header.contains("MOTD") => {
+                    println!("Recevied MOTD");
+                    println!("MOTD - {}", content);
+                }
+                Message { header, content } if header.contains("PRIVMSG") => {
+                    println!(
+                        "<{}> {}",
+                        header.split_whitespace().collect::<Vec<_>>()[0],
+                        content
+                    );
+                }
+                Message { content, .. } if content.contains("No Ident response") => {
+                    println!("Sending AUTH");
+                    send_auth(&conn, &mut stream)?;
+                }
+                Message { content, .. } if content.contains("376") => {
+                    println!("Joining channel");
+                    stream.write_all(&*format_cmd("JOIN", &conn.channel))?;
+                }
+                _ => continue,
             }
         }
     } else {
         println!("Could not connect to {}", &conn.server);
     }
-
     Ok(())
 }
