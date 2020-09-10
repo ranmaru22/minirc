c042fa06146944033c65bd6ad5d7f8ff07fba3f7
Alex Sun <alexsun82@icloud.com>
2020-09-10 18:51:07 +0200

    Add pub server field

--- src/connection.rs
+++ src/connection.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 pub struct Connection {
     pub address: String,
+    pub server: String,
     pub password: Option<String>,
     pub username: String,
 }
@@ -9,6 +10,7 @@ impl Connection {
     pub fn new(server: String, port: String, password: String, username: String) -> Self {
         Self {
             address: format!("{}:{}", server, port),
+            server,
             password: match password {
                 p if p.is_empty() => None,
                 p => Some(p),
